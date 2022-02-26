597ca0c5a1b67e8ab08e19a4308c54411413bfcd
Kevin-Souza <kevinsouza1.ks@gmail.com>
2022-02-26 19:48:23 -0300

    Adicionar arquivos de projeto.

---
+++ DesignPatterns/Controllers/SingletonController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DesignPatterns.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SingletonController : ControllerBase
+    {
+    }
+}
